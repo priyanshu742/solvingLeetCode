8dd49c0fbd5312e0426a62f06bf877b534c4c9c8
Priyanshu Singh <290099576+priyanshu742@users.noreply.github.com>
2026-08-12 19:29:44 +0530

    Time: 19 ms (83.51%), Space: 45.9 MB (46.87%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -2,24 +2,49 @@ class Solution
 {
     public List<List<Integer>> fourSum(int[] nums, int target) 
     {
-        // Better
+        // optimal
+        Arrays.sort(nums);
         Set<List<Integer>> result=new HashSet<>();
         for(int i=0;i<nums.length;i++)
         {
+            if(i>0 && nums[i]==nums[i-1])
+            {
+                continue;
+            }
             for(int j=i+1;j<nums.length;j++)
             {
-                Set<Long> temp=new HashSet<>();
-                for(int k=j+1;k<nums.length;k++)
+                if(j>i+1 && nums[j]==nums[j-1])
                 {
-                    long sum=(long)nums[i]+nums[j]+nums[k];
-                    long fourth=(long)target-(sum);
-                    if(temp.contains(fourth))
+                    continue;
+                }
+                int k=j+1;
+                int l=nums.length-1;
+                while(k<l)
+                {
+                    long sum=(long)nums[i]+nums[j]+nums[k]+nums[l];
+                    if(sum<target)
                     {
-                        List<Integer> ans=Arrays.asList(nums[i],nums[j],nums[k],(int)fourth);
-                        Collections.sort(ans);
-                        result.add(ans);
+                        k++;
                     }
-                    temp.add((long)nums[k]);
+                    else if(sum>target)
+                    {
+                        l--;
+                    }
+                    else
+                    {
+                        List<Integer> ans=Arrays.asList(nums[i],nums[j],nums[k],nums[l]);
+                        result.add(ans);
+                        k++;
+                        l--;
+                        while(k<l && nums[k]==nums[k-1])
+                        {
+                             k++;
+                        }
+                        while(k<l && nums[l]==nums[l+1])
+                        {
+                            l--;
+                        }
+                    }
                 }
             }
         }
