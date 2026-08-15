36cc48cfdc065a34d247979a147dd5300d44aee7
Priyanshu Singh <290099576+priyanshu742@users.noreply.github.com>
2026-08-15 17:42:53 +0530

    Time: 10 ms (22.32%), Space: 48.7 MB (92.68%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -11,34 +11,26 @@ class Solution
             return Integer.compare(a[0],b[0]);
         });
 
-        List<List<Integer>> result =new ArrayList<>();
+        List<List<Integer>> result=new ArrayList<>();
         for(int i=0;i<intervals.length;i++)
         {
-            int start=intervals[i][0];
-            int end=intervals[i][1];
-            if(!result.isEmpty() && end<=result.get(result.size()-1).get(1))
+            if(result.isEmpty() || intervals[i][0]>result.get(result.size()-1).get(1))
             {
-                continue;
+                result.add(Arrays.asList(intervals[i][0],intervals[i][1]));
             }
-            for(int j=i+1;j<intervals.length;j++)
+            else
             {
-                if(intervals[j][0]<=end)
-                {
-                    end=Math.max(intervals[j][1],end);
-                }
-                else
-                {
-                    break;
-                }
+               int currentMaxEnd=Math.max(intervals[i][1],result.get(result.size()-1).get(1));
+               result.get(result.size()-1).set(1,currentMaxEnd);
             }
-            result.add(Arrays.asList(start,end));
         }
-        int ans[][]=new int[result.size()][2];
+        int [][] main=new int [result.size()][2];
         for(int i=0;i<result.size();i++)
         {
-            ans[i][0]=result.get(i).get(0);
-            ans[i][1]=result.get(i).get(1);
+            main[i][0]=result.get(i).get(0);
+            main[i][1]=result.get(i).get(1);
         }
-        return ans;
+        return main;
     }
 }
+    
