839e02f0d0261da79e7d10624117192e037483df
Priyanshu Singh <290099576+priyanshu742@users.noreply.github.com>
2026-08-21 14:40:05 +0530

    Time: 0 ms (100%), Space: 48.4 MB (50.4%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -2,23 +2,28 @@ class Solution
 {
     public int search(int[] nums, int target) 
     {
-        int low=0;
-        int high=nums.length-1;
-        while(low<=high)
+        // recursive approach
+        int size=nums.length-1;
+        return binarySearch(nums,0,size,target);
+    }
+    public int binarySearch(int nums[],int low,int high,int target)
+    {
+        if(low>high)
         {
-            int mid=low+(high-low)/2;
-            if(nums[mid]==target)
-            {
-                return mid;
-            }
-            else if(target>nums[mid])
-            {
-                low=mid+1;
-            }
-            else if(target<nums[mid])
-            {
-                high=mid-1;
-            }
+            return -1;
+        }
+        int mid=low+(high-low)/2;
+        if(nums[mid]==target)
+        {
+            return mid;
+        }
+        else if(target>nums[mid])
+        {
+            return binarySearch(nums,mid+1,high,target);
+        }
+        else if(target<nums[mid])
+        {
+            return binarySearch(nums,low,mid-1,target);
         }
         return -1;
     }
