8f410f8ea29ee921bf1faed08b2a37d3c7c24c26
Priyanshu Singh <290099576+priyanshu742@users.noreply.github.com>
2026-08-27 18:43:48 +0530

    Time: 0 ms (100%), Space: 43.9 MB (28.16%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -2,64 +2,32 @@ class Solution
 {
     public int search(int[] nums, int k) 
     {
-        if(nums.length==0 || nums.length==1)
-        {
-            if(nums[0]==k)
-            {
-                return 0;
-            }
-            else
-            {
-                return -1;
-            }
-        }
         int low=0;
         int high=nums.length-1;
-        int count=0;
-        int ans=-1;
 
-        for(int i=0;i<nums.length-1;i++)
+        while(low<=high)
         {
-            if(nums[i]<=nums[i+1])
-            {
-                count++;
-            }
-            else
-            {
-                break;
-            }
-        }
-
-        int intermediate=count;
-        while(low<=intermediate)
-        {
-            int mid=low+(intermediate-low)/2;
+            int mid=low+(high-low)/2;
             if(nums[mid]==k)
             {
                 return mid;
             }
-            else if(nums[mid]<k)
+            else if(nums[low]<=nums[mid])
             {
-                low=mid+1;
+                if(nums[low]<=k && k<=nums[mid])
+                {
+                    high=mid-1;
+                }
+                else
+                {
+                    low=mid+1;
+                }
             }
             else
             {
-                intermediate=mid-1;
-            }
-        }
-        if(count<nums.length)
-        {
-            int midHigh=count+1;
-            while(midHigh<=high)
-            {
-                int mid=midHigh+(high-midHigh)/2;
-                if(nums[mid]==k)
+                if(nums[mid]<=k && k<=nums[high])
                 {
-                    return mid;
-                }
-                else if(nums[mid]<k)
-                {
-                    midHigh=mid+1;
+                    low=mid+1;
                 }
                 else
                 {
@@ -67,6 +35,6 @@ class Solution
                 }
             }
         }
-        return ans;
+        return -1;
     }
 }
